0724d1f4529a95e112101b901b8fcacf1b76a478
lipido <lipido@gmail.com>
2012-10-31 18:02:57 +0100

    Added json library.

--- jarvest/cmd/jarvest.sh
+++ jarvest/cmd/jarvest.sh
@@ -9,5 +9,5 @@ do
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-java -cp $DIR/../lib/commons-codec-1.3.jar:$DIR/../lib/commons-httpclient-3.1.jar:$DIR/../lib/commons-logging-1.1.1.jar:$DIR/../lib/htmlcleaner1_55.jar:$DIR/../lib/jruby-complete-1.6.7.jar:$DIR/../lib/yacli.jar:$DIR/../bin es.uvigo.ei.sing.jarvest.JarvestApp "$@"
+java -cp $DIR/../lib/json.jar:$DIR/../lib/commons-codec-1.3.jar:$DIR/../lib/commons-httpclient-3.1.jar:$DIR/../lib/commons-logging-1.1.1.jar:$DIR/../lib/htmlcleaner1_55.jar:$DIR/../lib/jruby-complete-1.6.7.jar:$DIR/../lib/yacli.jar:$DIR/../bin es.uvigo.ei.sing.jarvest.JarvestApp "$@"
 
