fb222e057d8ea20293a6439f37fef187c5d503a1
lipido <lipido@gmail.com>
2012-10-31 19:37:06 +0100

    Better detection of jARVEST directory

--- jarvest/cmd-dist/jarvest.sh
+++ jarvest/cmd-dist/jarvest.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-java -jar `dirname $0`/jarvest.jar "$@"
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+java -jar $DIR/jarvest.jar "$@"
 
