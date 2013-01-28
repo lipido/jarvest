ed0b096dbb1e1f338c875a81777f173f8b7a92e4
lipido <lipido@gmail.com>
2013-01-29 00:57:03 +0100

    Fixing jarvest.sh to take command line parameters

--- jarvest/cmd/jarvest.sh
+++ jarvest/cmd/jarvest.sh
@@ -10,5 +10,5 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 #java -cp $DIR/../lib/json.jar:$DIR/../lib/jsoup-1.7.1.jar:$DIR/../lib/commons-codec-1.3.jar:$DIR/../lib/commons-httpclient-3.1.jar:$DIR/../lib/commons-logging-1.1.1.jar:$DIR/../lib/htmlcleaner1_55.jar:$DIR/../lib/jruby-complete-1.6.7.jar:$DIR/../lib/yacli.jar:$DIR/../bin es.uvigo.ei.sing.jarvest.JarvestApp "$@"
-java -jar $DIR/../target/jarvest-*-with-dependencies.jar
+java -jar $DIR/../target/jarvest-*-with-dependencies.jar "$@"
 
